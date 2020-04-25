b27d4074dbc255ac569f214294e4427c5eff65a9
tangtang-zhou <294370753@qq.com>
2020-04-25 11:57:13 +0800

    登录

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import './plugins/element.js'
+import './assets/css/global.css'
+import axios from 'axios'
+
+Vue.prototype.$http = axios
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
---
+++ src/router/index.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Login from '../components/login'
+import Home from '../components/home'
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: '/login',
+    component: Login
+  },
+  {
+    path: '/',
+    redirect: '/login'
+  },
+  {
+    path: '/home',
+    component: Home
+  }
+];
+
+const router = new VueRouter({
+  routes
+});
+
+export default router;
